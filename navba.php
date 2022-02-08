0e19d448038eea3e6381b24ec026bf4b3c78203f
Kevomwass <96240376+Kevomwass@users.noreply.github.com>
2022-02-08 12:17:05 -0800

    nav bar

---
+++ navba.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/style.css"  rel="stylesheet">
+    <title>nav bar</title>
+</head>
+<body>
+<div class="navbar">
+<h2>creating navigation bar</h2>
+    <ul>
+    <li><a href="containers.php">Home</a></li>
+    <li><a href="introduction.php">About us</a></li>
+    <li><a href="mypractice.php">contact us</a></li>
+    <li><a href="webdevintro.php">make an appointment</a></li>
+    </ul>
+</div>
+
+</body>
+</html>
